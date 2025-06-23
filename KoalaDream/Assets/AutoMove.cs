1e05456ee19e240b7e62754c7ba27514b8d0e5f7
Denis Khasanov <denishasanov828@gmail.com>
2025-06-23 23:37:41 +0500

    0.0.1.7

---
+++ KoalaDream/Assets/AutoMove.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMove : MonoBehaviour
+{
+    [SerializeField] private List<Transform> tests = new List<Transform>();
+
+    [SerializeField] private PlayerMove playerMove;
+
+    [SerializeField] private bool isActive = false;
+    [SerializeField] private float stopTreshold = 0.05f;
+
+    private Transform currentTarget;
+
+    private void Awake()
+    {
+        playerMove.OnPositionChanged += OnPositionChanged;
+    }
+
+    private void OnDestroy()
+    {
+        playerMove.OnPositionChanged -= OnPositionChanged;
+    }
+
+    public void MoveTo(Transform target)
+    {
+        currentTarget = target;
+        isActive = true;
+        UpdateDirection();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            MoveTo(tests[0]);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            MoveTo(tests[1]);
+        }
+    }
+
+    private void OnPositionChanged(float currentX)
+    {
+        if(!isActive) return;
+
+        if(currentTarget == null) return;
+
+        float diff = currentTarget.position.x - currentX;
+
+        if(Mathf.Abs(diff) < stopTreshold)
+        {
+            Cancel();
+        }
+    }
+
+    private void UpdateDirection()
+    {
+        if(currentTarget == null)
+        {
+            Cancel();
+            return;
+        }
+
+        float diff = currentTarget.position.x - playerMove.transform.position.x;
+
+        if(Mathf.Abs(diff) < stopTreshold)
+        {
+            Cancel();
+            return;
+        }
+
+        playerMove.Move(Mathf.Sign(diff));
+    }
+
+    private void Cancel()
+    {
+        isActive = false;
+        playerMove.Move(0);
+    }
+}
---
+++ KoalaDream/Assets/PlayerMove.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Transform leftBoundary;
+    public Transform rightBoundary;
+
+    public float maxSpeed;
+    public float accelerationTime = 0.2f;
+    public float decelerationTime = 0.2f;
+
+    private float currentSpeed = 0;
+
+    [Range(-1, 1)]
+    public float targetDirection = 90;
+
+    private void Update()
+    {
+        float targetSpeed = targetDirection * maxSpeed;
+
+        if(Mathf.Abs(targetSpeed) > Mathf.Abs(currentSpeed))
+            currentSpeed = Mathf.MoveTowards(currentSpeed, targetSpeed, maxSpeed / accelerationTime * Time.deltaTime);
+        else
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, targetSpeed, maxSpeed / decelerationTime * Time.deltaTime);
+        }
+
+        Vector3 pos = transform.position;
+
+        float minX = leftBoundary.position.x;
+        float maxX = rightBoundary.position.x;
+
+        pos.x += currentSpeed * Time.deltaTime;
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+
+        if((pos.x == minX  && currentSpeed < 0) || (pos.x == maxX && currentSpeed > 0))
+        {
+            currentSpeed = 0f;
+        } 
+
+        transform.position = pos;
+
+        OnPositionChanged?.Invoke(pos.x);
+    }
+
+    public void Move(float direction)
+    {
+        targetDirection = Mathf.Clamp(direction, -1, 1);
+    }
+
+    #region Output
+
+    public event Action<float> OnPositionChanged;
+
+    #endregion
+}
