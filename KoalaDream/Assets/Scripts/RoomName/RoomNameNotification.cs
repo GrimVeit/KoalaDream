d4229a0dd46ed469af408532cb0ecc496fdbefd4
Denis Khasanov <denishasanov828@gmail.com>
2025-07-03 23:01:19 +0500

    0.0.3.5.1

--- KoalaDream/Assets/Scripts/RoomName/RoomNameNotification.cs
+++ KoalaDream/Assets/Scripts/RoomName/RoomNameNotification.cs
@@ -12,13 +12,19 @@ public class RoomNameNotification : MonoBehaviour
 
     private Transform _transformEnd;
 
-    private IEnumerator timer;
+    private IEnumerator timerWait;
+    private IEnumerator timerTypeText;
 
     private Sequence sequence;
 
+    private string _roomName;
+    private float _durationTextTyping;
+
     public void SetData(string roomName, float durationTextTyping, Transform transformEnd)
     {
-        textName.text = roomName;
+        _roomName = roomName;
+        _durationTextTyping = durationTextTyping;
+
         _transformEnd = transformEnd;
     }
 
@@ -28,19 +34,24 @@ public class RoomNameNotification : MonoBehaviour
 
         sequence = DOTween.Sequence();
 
+
         sequence
-            .Append(transform.DOLocalMove(transformDown.localPosition, durationMove))
+            .Append(transform.DOLocalMove(transformDown.localPosition, durationMove).SetEase(Ease.InOutBounce, 0.1f))
             .Join(textName.DOFade(1, durationMove));
 
-        if(timer != null) Coroutines.Stop(timer);
+        if(timerWait != null) Coroutines.Stop(timerWait);
+        if(timerTypeText != null) Coroutines.Stop(timerTypeText);
 
-        timer = Timer();
-        Coroutines.Start(timer);
+        timerWait = Timer();
+        Coroutines.Start(timerWait);
+
+        timerTypeText = AddText();
+        Coroutines.Start(timerTypeText);
     }
 
     public void Deactivate()
     {
-        if (timer != null) Coroutines.Stop(timer);
+        if (timerWait != null) Coroutines.Stop(timerWait);
 
         sequence?.Kill();
 
@@ -53,6 +64,9 @@ public class RoomNameNotification : MonoBehaviour
 
     private void OnDestroy()
     {
+        if (timerWait != null) Coroutines.Stop(timerWait);
+        if (timerTypeText != null) Coroutines.Stop(timerTypeText);
+
         sequence?.Kill();
     }
 
@@ -62,4 +76,37 @@ public class RoomNameNotification : MonoBehaviour
 
         Deactivate();
     }
+
+    private IEnumerator AddText()
+    {
+        textName.text = "";
+
+        foreach(char c in _roomName)
+        {
+            textName.text += c;
+
+            yield return new WaitForSeconds(_durationTextTyping);
+        }
+
+        yield return new WaitForSeconds(0.7f);
+
+        textName.text = _roomName;
+
+        for (int i = _roomName.Length - 1; i >= 0; i--)
+        {
+            textName.text = textName.text.Substring(0, i);
+            yield return new WaitForSeconds(_durationTextTyping);
+        }
+    }
+
+    //private IEnumerator RemoveText()
+    //{
+    //    textName.text = _roomName;
+
+    //    for (int i = _roomName.Length - 1; i >= 0; i--)
+    //    {
+    //        textName.text = textName.text.Substring(0, i);
+    //        yield return new WaitForSeconds(_durationTextTyping);
+    //    }
+    //}
 }
